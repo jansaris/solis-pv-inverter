be250226965c5f85c3da1d5440ee9c239928be10
Jan Saris <jan.saris@gmail.com>
2022-01-06 13:48:48 +0100

    Fixed regex and comments

--- solis.py
+++ solis.py
@@ -1,9 +1,9 @@
 """Solis PV Converter integration API."""
+import asyncio
 import logging
+import re
 
 import aiohttp
-import asyncio
-import re
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -11,17 +11,18 @@ _LOGGER = logging.getLogger(__name__)
 class Solis:
     """Solis PV Converter integration API."""
 
-    POWER_REGEX = re.compile(r'var webdata_now_p = "([0-9]+(\.[0-9]+)?)"')
-    TODAY_REGEX = re.compile(r'var webdata_today_e = "([0-9]+(\.[0-9]+)?)"')
-    TOTAL_REGEX = re.compile(r'var webdata_total_e = "([0-9]+(\.[0-9]+)?)"')
+    POWER_REGEX = re.compile(r'var webdata_now_p = "(\d+(\.\d+)*)"')
+    TODAY_REGEX = re.compile(r'var webdata_today_e = "(\d+(\.\d+)*)"')
+    TOTAL_REGEX = re.compile(r'var webdata_total_e = "(\d+(\.\d+)*)"')
 
     def __init__(self, url, username, password):
+        """Initialize Solis PV Converter integration."""
         self._url = f"{url}/status.html"
         self._username = username
         self._password = password
 
     async def retrieve(self):
-        """Retrieves the model from the Solis PV internal website."""
+        """Retrieve the model from the Solis PV internal website."""
         _LOGGER.debug("Retrieve data from %s", self._url)
         async with aiohttp.ClientSession(
             auth=aiohttp.BasicAuth(self._username, self._password)
@@ -44,7 +45,7 @@ class Solis:
 
     @staticmethod
     def _extract_model(html):
-        """Extract the complete model from the HTML using regex"""
+        """Extract the complete model from the HTML using regex."""
         power = Solis._extract_regex_value(Solis.POWER_REGEX, html)
         today = Solis._extract_regex_value(Solis.TODAY_REGEX, html)
         total = Solis._extract_regex_value(Solis.TOTAL_REGEX, html)
@@ -52,7 +53,7 @@ class Solis:
 
     @staticmethod
     def _extract_regex_value(regex, html):
-        """Extract the value from the HTML using regex"""
+        """Extract the value from the HTML using regex."""
         match = regex.search(html)
         if match:
             return match.group(1)
@@ -64,25 +65,26 @@ class SolisModel:
 
     @classmethod
     def unreachable(cls):
-        """Creates an error model for Host unreachable."""
+        """Create an error model for Host unreachable."""
         return cls("Host unreachable", 0, False, True, 0, 0, 0)
 
     @classmethod
     def create(cls, power, today, total):
-        """Creates a valid model with given data."""
+        """Create a valid model with given data."""
         return cls("OK", 200, True, False, power, today, total)
 
     @classmethod
     def connection_error(cls):
-        """Creates an error model for a connection error."""
+        """Create an error model for a connection error."""
         return cls("Connection error", 0, False, True, 0, 0, 0)
 
     @classmethod
     def http_error(cls, http_code, reason):
-        """Creates an error model with the http status code."""
+        """Create an error model with the http status code."""
         return cls(reason, http_code, True, True, 0, 0, 0)
 
     def __init__(self, status, http_code, reachable, error, power, today, total):
+        """Initialize Solis Model."""
         self.reachable = reachable
         self.error = error
         self.current_power = power
